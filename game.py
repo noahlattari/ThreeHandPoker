4133ca21cfa3d9e54bf55478bc948b21d80e3b47
noahlattari <zennoah1998@gmail.com>
2019-10-02 23:10:44 -0400

    mapps

--- game.py
+++ game.py
@@ -1,7 +1,18 @@
-import sys
+mappings = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+#print(mappings.get('T'))
 
+
+bestHand = []
 lines = []
-number_of_hands = input()
-for i in range(int(number_of_hands)):
+hands = input()
+for i in range(int(hands)):
     lines+=input().replace("\n", "").split(" ")
-print (lines)
+
+bestHand = lines[0] + lines[1] + lines[2] + lines[3] 
+
+#for i in range( int(lines) / hands - 1) :
+
+
+def highCard( hand ):
+    # hand = x1y2z3
+    if (hand[0])
