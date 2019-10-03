26a2766d15be3e36fe9656ff182b918ae6f17356
noahlattari <zennoah1998@gmail.com>
2019-10-02 23:20:18 -0400

    er33

--- game.py
+++ game.py
@@ -4,15 +4,16 @@ mappings = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':
 
 bestHand = []
 lines = []
-hands = input()
-for i in range(int(hands)):
+number_of_hands = input()
+for i in range(int(number_of_hands)):
     lines+=input().replace("\n", "").split(" ")
 
-bestHand = lines[0] + lines[1] + lines[2] + lines[3] 
+bestHand = [ lines[0], lines[1], lines[2], lines[3]]
 
 #for i in range( int(lines) / hands - 1) :
 
+print(lines)
+print(bestHand)
 
 def highCard( hand ):
-    # hand = x1y2z3
-    if (hand[0])
+   return max(hand[0], hand[1], hand[2])
