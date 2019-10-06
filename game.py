b58d73b69fce3aa95c783b8806184329f09c196b
noahlattari <zennoah1998@gmail.com>
2019-10-06 16:33:52 -0400

    3d32

--- game.py
+++ game.py
@@ -6,12 +6,6 @@ bestHand = []
 lines = []
 number_of_hands = input()
 
-for i in range(int(number_of_hands)):
-    lines+=input().replace("\n", "").split(" ")
-
-bestHand = [ lines[0], lines[1], lines[2], lines[3] ]
-
-
 def highCard( hand ):
    return max(mappings.get(hand[1][0]), mappings.get(hand[2][0]), mappings.get(hand[3][0]))
 
@@ -25,5 +19,35 @@ def straight(hand):
     if (x + 1 == y and y + 1 == z) or (x + 1 == z and z + 1 == y) or (y + 1 == z and z + 1 == x) or (y + 1 == x and x + 1 == z) or (z + 1 == y and y + 1 == x) or (z + 1 == x and x + 1 == y):
         return True
    
+def threeOfAKind(hand):
+   return mappings.get(hand[1][0]) == mappings.get(hand[2][0]) and mappings.get(hand[2][0]) == mappings.get(hand[3][0])
+
+def pair(hand):
+   x = mappings.get(hand[1][0])
+   y = mappings.get(hand[2][0])
+   z = mappings.get(hand[3][0])
+   if (x == y or x == z or y == z):
+      return True
 
 #returns an int based on the hand power
+def handType(hand):
+    if flush(hand) and straight(hand):
+        return 5
+    elif threeOfAKind(hand):
+        return 4
+    elif straight(hand):
+        return 3
+    elif flush(hand):
+        return 2
+    elif pair(hand):
+        return 1
+    else:
+        return 0
+
+
+for i in range(int(number_of_hands)):
+    lines+=input().replace("\n", "").split(" ")
+
+bestHand = [ lines[0], lines[1], lines[2], lines[3] ]
+
+
